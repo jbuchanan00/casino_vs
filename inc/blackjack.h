c3998a1939151dbf191b23f236d0a88b8cc1a324
Josh Buchanan <josh.buchanan00@gmail.com>
2022-03-26 21:17:12 -0400

    deleted dealer class, just integrate into blackjack class.  added some methods in blackjack class, need to implement them though

--- inc/blackjack.h
+++ inc/blackjack.h
@@ -1,8 +1,34 @@
 #ifndef BLACKJACK_H
 #define BLACKJACK_H
+#include <array>
+#include <string>
+#include <vector>
 
 class Blackjack {
-
+private:
+    std::array<std::string, 52> deck = { "hA", "hK", "hQ", "hJ",
+                                        "h10", "h9", "h8", "h7",
+                                        "h6", "h5", "h4", "h3",
+                                        "h2", "dA", "dK",
+                                        "dQ", "dJ","d10", "d9",
+                                        "d8", "d7","d6", "d5",
+                                        "d4", "d3","d2",
+                                        "cA", "cK", "cQ", "cJ",
+                                        "c10", "c9", "c8", "c7",
+                                        "c6", "c5", "c4", "c3",
+                                        "c2", "sA", "sK",
+                                        "sQ", "sJ", "s10", "s9",
+                                        "s8", "s7", "s6", "s5",
+                                        "s4", "s3", "s2" };
+protected:
+    std::vector<std::string> player_cards;
+    std::vector<std::string> dealer_cards;
+public:
+    Blackjack();
+    ~Blackjack() {}
+    void deal();
+    void hit();
+    void stand();
 };
 
 
--- src/blackjack.cpp
+++ src/blackjack.cpp
@@ -1,1 +1,17 @@
 #include "blackjack.h"
+
+Blackjack::Blackjack() {
+
+}
+
+void Blackjack::deal() {
+
+}
+
+void Blackjack::hit() {
+
+}
+
+void Blackjack::stand() {
+
+}
